532fc960f700f5c5a274cd37fcb05d039b507c56
Spencer Dearman <121642493+spencerdearman@users.noreply.github.com>
2024-11-05 23:31:12 +0000

    Finished

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def backdoor
+    render ({ :template => "item_templates/backdoor"})
+  end
+
+  def create
+    i = Item.new
+    i.link_url = params.fetch("link_url")
+    i.link_description = params.fetch("link_description")
+    i.thumbnail_url = params.fetch("thumbnail_url")
+    i.save
+    redirect_to("/")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "backdoor"})
+  post("/insert_item", { :controller => "items", :action => "create" })
 end
